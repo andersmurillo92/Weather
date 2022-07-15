56f19c293f0454b4a43b17e2e2ba9b29381bb196
John Anders Murillo <jamsmurillo92@gmail.com>
2022-07-15 12:58:17 -0500

    Create and implement detailed search activity

---
+++ app/src/main/java/com/andersmurillo/weather/views/searcher/SearchDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.andersmurillo.weather.views.searcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andersmurillo.weather.R
+
+class SearchDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_details)
+    }
+}
