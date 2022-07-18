d48052a24d57a3b6475110b8da35c709a944487f
John Anders Murillo <jamsmurillo92@gmail.com>
2022-07-18 15:45:32 -0500

    Create and implement needed methods for getting weather

--- app/src/main/java/com/andersmurillo/weather/views/searcher/SearchDetailsActivity.kt
+++ app/src/main/java/com/andersmurillo/weather/views/searcher/SearchDetailsActivity.kt
@@ -2,11 +2,40 @@ package com.andersmurillo.weather.views.searcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.andersmurillo.weather.R
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.andersmurillo.weather.databinding.ActivitySearchDetailsBinding
+import com.andersmurillo.weather.domain.model.Location
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchDetailsActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "SearchDetailsActivity"
+    }
+
+    private lateinit var binding: ActivitySearchDetailsBinding
+    private val viewModel:SearchViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_details)
+        binding = ActivitySearchDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+        initViewModelObservables()
+    }
+
+    private fun initView() {
+        val locationSelected = intent?.getSerializableExtra("item") as Location
+        viewModel.getLocationWeather(locationSelected.name)
+    }
+
+    private fun initViewModelObservables(){
+        viewModel.locationWeather.observe(this, Observer {
+            Log.i(TAG, "$it")
+        })
     }
 }
--- app/src/main/java/com/andersmurillo/weather/views/searcher/SearchViewModel.kt
+++ app/src/main/java/com/andersmurillo/weather/views/searcher/SearchViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.andersmurillo.weather.domain.GetLocationsUseCase
+import com.andersmurillo.weather.domain.GetWeatherUseCase
 import com.andersmurillo.weather.domain.model.Location
+import com.andersmurillo.weather.domain.model.Weather
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -13,7 +15,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val getLocationsUseCase: GetLocationsUseCase
+    private val getLocationsUseCase: GetLocationsUseCase,
+    private val getWeatherUseCase: GetWeatherUseCase
 ) : ViewModel() {
 
     companion object{
@@ -21,6 +24,7 @@ class SearchViewModel @Inject constructor(
     }
 
     val locationsModel = MutableLiveData<List<Location>>()
+    val locationWeather = MutableLiveData<Weather>()
     val isLoading = MutableLiveData<Boolean>()
 
     init {
@@ -40,6 +44,14 @@ class SearchViewModel @Inject constructor(
             } catch (ex:Exception) {
                 Log.i(TAG, ex.toString())
             }
+        }
+    }
+
+    fun getLocationWeather(query:String) {
+        viewModelScope.launch {
+            val result: Weather? = getWeatherUseCase(query)
+            if(result!=null)
+                locationWeather.value = result!!
         }
     }
 }
